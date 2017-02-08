29ebf243331d7bb522700cb9a380d5985d311d61
TaHuC <TaHuC@hotmail.com>
2017-02-08 23:06:18 +0200

    words finish randomize

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace RandomizeWords
+{
+	public class MainClass
+	{
+		public static void Main()
+		{
+			var words = Console.ReadLine()
+							   .Split(' ')
+			                   .ToList();
+
+			var randomize = new Random();
+
+			for (var i = 0; i < words.Count; i++)
+			{
+				var currentWord = words[i];
+
+				var newPos = randomize.Next(words.Count);
+
+				var tempWord = words[newPos];
+
+				words[i] = tempWord;
+
+				words[newPos] = currentWord;
+			}
+
+			Console.WriteLine(string.Join("\n", words));
+		}
+	}
+}
